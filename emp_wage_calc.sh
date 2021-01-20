d3bcef7f04d2156e97e767de720b73d2e8ad8072
cyril joseph <cyrilvj94@gmail.com>
2021-01-20 10:48:06 +0530

    Updated code using functions. Solved using case statement

--- emp_wage_calc.sh
+++ emp_wage_calc.sh
@@ -1,41 +1,54 @@
-#! /bin/bash
+#! /bin/bash 
 echo "Welcome to Employee wage computation program"
 
 #Check employee is present or absent
+function checkAttendance
+{
 isPresent=1
 attendance=$((RANDOM%2))
 if [ $attendance -eq $isPresent ]
 then
-	echo "Employee Present"
+	echo 1
 else
-	echo "Employee Absent"
+	echo 0
 fi
-
-#Calculate daily employee wage
-wagePerHr=20
-fullDayHr=8
-if [ $attendance -eq $isPresent ]
-then
-	salary=$((wagePerHr*fullDayHr))
-else
-	salary=0
-fi
-echo "Salary for employee :" $salary 
-
-#Add part time employee and wage
+}
+#Check if employee is parttime or full time
+function checkParttime
+{
 isPartTime=1
 random=$((RANDOM%2))
 if [ $random -eq $isPartTime ]
 then
-	echo "Part time employee"
+        echo 1
 else
-	echo "Not part time employee"
+        echo 0
 fi
-partDayHr=8
-if [ $isPartTime -eq $random ]
-then
-	part_salary=$((wagePerHr*partDayHr))
-else
-	part_salary=0
-fi
-echo "Part time salary : "$part_salary
+}
+
+#Calculate daily employee wage
+function empWage
+{
+attendance=$1
+isPartTime=$2
+isPresent=1
+wagePerHr=20
+fullDayHr=8
+partTimeHr=4
+case $attendance in 
+	0)salary=0;;
+	1)if [ $isPartTime -eq 1 ] ; then salary=$((wagePerHr*partTimeHr)) ; else salary=$((wagePerHr*fullDayHr)); fi ;  ;;
+esac
+echo $salary
+}
+#Main program
+attendance=$(checkAttendance);partTime=$(checkParttime)
+case $attendance in
+	0)echo Employee absent;;
+	1)echo Employee Present;;
+esac
+case $partTime in
+	0)echo "Full time employee";;
+	1)echo  "Part time employee"
+esac
+echo "Employee wage": $(empWage $attendance $partTime)
