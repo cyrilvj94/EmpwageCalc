8d53ed0a5f726c4e97975d930fda51fb3101520e
cyril joseph <cyrilvj94@gmail.com>
2021-01-20 08:48:49 +0530

    Added Employee attendence check

--- emp_wage_calc.sh
+++ emp_wage_calc.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 echo "Welcome to Employee wage computation program"
 
+#Check employee is present or absent
+isPresent=1
+attendance=$((RANDOM%2))
+if [ $attendance -eq $isPresent ]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
+
+
